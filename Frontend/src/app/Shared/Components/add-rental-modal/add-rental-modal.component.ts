d3d6c636319401c5581ac4a447ddb0928f6e0d8c
Keelan-Matthews <keelanm@agilebridge.co.za>
2024-02-05 20:13:16 +0200

    Can successfully make a rental

--- Frontend/src/app/Shared/Components/add-rental-modal/add-rental-modal.component.ts
+++ Frontend/src/app/Shared/Components/add-rental-modal/add-rental-modal.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatLabel } from '@angular/material/form-field';
 import { MatOption } from '@angular/material/core';
-import {MatFormField} from '@angular/material/form-field';
-import {MatDatepickerControl, MatDatepickerModule, MatDatepickerPanel} from '@angular/material/datepicker';
+import { MatFormField } from '@angular/material/form-field';
+import {
+  MatDatepickerControl,
+  MatDatepickerModule,
+  MatDatepickerPanel,
+} from '@angular/material/datepicker';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { RentalService } from '../../Core/Services/rental/rental.service';
 import { CarService } from '../../Core/Services/car/car.service';
@@ -14,68 +18,80 @@ import { map } from 'rxjs';
 @Component({
   selector: 'app-add-rental-modal',
   templateUrl: './add-rental-modal.component.html',
-  styleUrl: './add-rental-modal.component.scss'
+  styleUrl: './add-rental-modal.component.scss',
 })
 export class AddRentalModalComponent {
   form = new FormGroup({});
-  model = { };
-  fields: FormlyFieldConfig[] = [];
+  model = {};
 
+  constructor(
+    public dialogRef: MatDialogRef<AddRentalModalComponent>,
+    private _rentalService: RentalService,
+    private _carService: CarService,
+    private _clientService: ClientService
+  ) {}
 
-  constructor(public dialogRef: MatDialogRef<AddRentalModalComponent>, private _rentalService:RentalService,
-    private _carService:CarService, private _clientService:ClientService) {}
-
-
-  loadFormFields() {
-    // Assuming getCars and getClients are methods that fetch the data
-    const carOptions = this._carService.getAllCars().pipe(map(cars => cars.map(car => ({ label: car.registration, value: car.id }))));
-    const clientOptions = this._clientService.getAllClients().pipe(map(clients => clients.map(client => ({ label: client.firstName + ' ' + client.lastName, value: client.id }))));
-
-    this.fields = [
-      {
-        key: 'carId',
-        type: 'select',
-        props: {
-          label: 'Select Car',
-          placeholder: 'Select a car',
-          required: true,
-          options: carOptions,
-        },
+  private carOptions = this._carService
+    .getAllCars()
+    .pipe(
+      map((cars) =>
+        cars.map((car) => ({ label: car.registration, value: car.id }))
+      )
+    );
+  private clientOptions = this._clientService
+    .getAllClients()
+    .pipe(
+      map((clients) =>
+        clients.map((client) => ({
+          label: client.firstName + ' ' + client.lastName,
+          value: client.id,
+        }))
+      )
+    );
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'carId',
+      type: 'select',
+      props: {
+        label: 'Select Car',
+        placeholder: 'Select a car',
+        required: true,
+        options: this.carOptions,
       },
-      {
-        key: 'clientId',
-        type: 'select',
-        props: {
-          label: 'Select Client',
-          placeholder: 'Select a client',
-          required: true,
-          options: clientOptions,
-        },
+    },
+    {
+      key: 'clientId',
+      type: 'select',
+      props: {
+        label: 'Select Client',
+        placeholder: 'Select a client',
+        required: true,
+        options: this.clientOptions,
       },
-      {
-        key: 'startDate',
-        type: 'datepicker',
-        props: {
-          label: 'Start Date',
-          placeholder: 'Select start date',
-          required: true,
-        },
+    },
+    {
+      key: 'startDate',
+      type: 'datepicker',
+      props: {
+        label: 'Start Date',
+        placeholder: 'Select start date',
+        required: true,
       },
-      {
-        key: 'endDate',
-        type: 'datepicker',
-        props: {
-          label: 'End Date',
-          placeholder: 'Select end date',
-          required: true,
-        },
+    },
+    {
+      key: 'endDate',
+      type: 'datepicker',
+      props: {
+        label: 'End Date',
+        placeholder: 'Select end date',
+        required: true,
       },
-    ];
-  }
+    },
+  ];
 
   onSubmit(model: any): void {
-      this._rentalService.createRental(model);
-      this.close();
+    this._rentalService.createRental(model);
+    this.close();
   }
 
   close(): void {
