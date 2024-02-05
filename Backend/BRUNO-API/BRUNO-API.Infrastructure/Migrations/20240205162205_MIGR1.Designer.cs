328f0cbcc15e437368560c5cfd39fd7358ce66d1
Jacquesvdw02 <jacquesvdw@agilebridge.co.za>
2024-02-05 18:29:30 +0200

    a

--- Backend/BRUNO-API/BRUNO-API.Infrastructure/Migrations/20240205162205_MIGR1.Designer.cs
+++ Backend/BRUNO-API/BRUNO-API.Infrastructure/Migrations/20240205162205_MIGR1.Designer.cs
@@ -4,6 +4,7 @@ using BRUNOAPI.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BRUNOAPI.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240205162205_MIGR1")]
+    partial class MIGR1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/BRUNO-API/BRUNO-API.Infrastructure/Migrations/20240205162205_MIGR1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BRUNOAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MIGR1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ClientName",
+                table: "Clients",
+                newName: "Province");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateJoined",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BodyStyle",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DatePurchased",
+                table: "Cars",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Drivetrain",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "EngineSize",
+                table: "Cars",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FuelType",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Transmission",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "DateJoined",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "BodyStyle",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "DatePurchased",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Drivetrain",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "EngineSize",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "FuelType",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Transmission",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Cars");
+
+            migrationBuilder.RenameColumn(
+                name: "Province",
+                table: "Clients",
+                newName: "ClientName");
+        }
+    }
+}
