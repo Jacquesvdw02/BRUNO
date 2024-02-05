73325146bbcf8cbae7a3b677e8bc30cfcaff2d5d
Keelan-Matthews <keelanm@agilebridge.co.za>
2024-02-05 17:16:39 +0200

    Updated forms to use formly

--- Frontend/src/app/Shared/Components/add-rental-modal/add-rental-modal.component.ts
+++ Frontend/src/app/Shared/Components/add-rental-modal/add-rental-modal.component.ts
@@ -5,43 +5,33 @@ import { MatLabel } from '@angular/material/form-field';
 import { MatOption } from '@angular/material/core';
 import {MatFormField} from '@angular/material/form-field';
 import {MatDatepickerControl, MatDatepickerModule, MatDatepickerPanel} from '@angular/material/datepicker';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-add-rental-modal',
   templateUrl: './add-rental-modal.component.html',
   styleUrl: './add-rental-modal.component.scss'
 })
-export class AddRentalModalComponent implements OnInit {
-  public entryForm!: FormGroup;
-  public carOptions!: string[];
-  public clientOptions!: string[];
-  public startDate!: MatDatepickerPanel<MatDatepickerControl<any>,any,any>;
-  public endDate!: MatDatepickerPanel<MatDatepickerControl<any>,any,any>;
-
-  constructor(
-    private dialogRef: MatDialogRef<AddRentalModalComponent>,
-    private formBuilder: FormBuilder
-  ) { }
-
-  ngOnInit(): void {
-    this.entryForm = this.formBuilder.group({
-      carOptions: ['', Validators.required],
-      clientOptions: ['', Validators.required],
-      startDate: ['', Validators.required],
-      endDate: ['', Validators.required],
-    });
-  }
-
-  get fc() {
-    return this.entryForm.controls;
-  }
-
-  submit(): void {
-    if (this.entryForm?.valid) {
-      // You can handle form submission here
-      console.log('Form submitted:', this.entryForm.value);
-      this.close();
+export class AddRentalModalComponent {
+  form = new FormGroup({});
+  model = { };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      props: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true,
+      }
     }
+  ];
+
+  constructor(public dialogRef: MatDialogRef<AddRentalModalComponent>) {}
+
+  onSubmit(model: any): void {
+      console.log('Form submitted:', model);
+      this.close();
   }
 
   close(): void {
