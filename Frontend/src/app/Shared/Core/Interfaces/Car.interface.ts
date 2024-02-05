1b746b9d52d6a310bc94fe38d2e485d9823bda9b
Keelan-Matthews <keelanm@agilebridge.co.za>
2024-02-05 16:23:30 +0200

    Updated car interface

--- Frontend/src/app/Shared/Core/Interfaces/Car.interface.ts
+++ Frontend/src/app/Shared/Core/Interfaces/Car.interface.ts
@@ -5,6 +5,14 @@ export interface Car {
     make: string;
     mileage: number;
     model: string;
+    brand: string;
+    year: number;
+    transmission: string;
+    fuelType: 'Petrol' | 'Gasoline' | 'Electric' | 'Hybrid';
+    engineSize: number;
+    bodyStyle: string;
+    driveTrain: string;
+    datePurchased: Date;
     registration: string;
     rentedOut: boolean;
     serviceMileage: number; // Updated to match Swagger
